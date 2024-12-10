32844845dfd2c313c775a9060b03ead6558d88fd
KashyapChilivoju <kchilivoju1@gmail.com>
2024-12-10 20:45:34 +1100

    Redefined Schema to seperate Cars from Stock | Dummy Data Added

---
+++ Database/data.sql
@@ -0,0 +1,140 @@
+INSERT INTO Dealers (DealerName, DealerToken) VALUES
+    ('Prime Auto Group', 'token123'),
+    ('Luxury Wheels Inc.', 'token123'),
+    ('Family Motors', 'token123'),
+    ('EcoDrive Auto', 'token123');                
+
+
+INSERT INTO Cars (Make, Model, Year, Colours, Body, Transmission, FuelType, Seats, Doors) VALUES
+    -- Toyota Camry Variations
+    ('Toyota', 'Camry', 2000, 'Red,Blue', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Toyota', 'Camry', 2005, 'Silver,Black', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Toyota', 'Camry', 2010, 'Gray,White', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('Toyota', 'Camry', 2015, 'Blue,Red', 'Sedan', 'Manual', 'Petrol', 5, 4),
+    ('Toyota', 'Camry', 2020, 'White,Silver', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    
+    -- Honda Civic Variations
+    ('Honda', 'Civic', 2001, 'Black,Gray', 'Sedan', 'Manual', 'Petrol', 5, 4),
+    ('Honda', 'Civic', 2007, 'White,Blue', 'Sedan', 'Automatic', 'Diesel', 5, 4),
+    ('Honda', 'Civic', 2013, 'Gray,Red', 'Sedan', 'Manual', 'Petrol', 5, 4),
+    ('Honda', 'Civic', 2018, 'Blue,Black', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Honda', 'Civic', 2022, 'White,Silver', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    
+    -- Tesla Model S Variations
+    ('Tesla', 'Model S', 2012, 'White,Silver', 'Sedan', 'Automatic', 'Electric', 5, 4),
+    ('Tesla', 'Model S', 2015, 'Black,Blue', 'Sedan', 'Automatic', 'Electric', 5, 4),
+    ('Tesla', 'Model S', 2018, 'Gray,White', 'Sedan', 'Automatic', 'Electric', 5, 4),
+    ('Tesla', 'Model S', 2021, 'Silver,Black', 'Sedan', 'Automatic', 'Electric', 5, 4),
+    ('Tesla', 'Model S', 2023, 'Red,Blue', 'Sedan', 'Automatic', 'Electric', 5, 4),
+    
+    -- Ford Focus Variations
+    ('Ford', 'Focus', 2002, 'Blue,Gray', 'Hatchback', 'Manual', 'Petrol', 5, 4),
+    ('Ford', 'Focus', 2006, 'Black,White', 'Hatchback', 'Automatic', 'Diesel', 5, 4),
+    ('Ford', 'Focus', 2011, 'White,Red', 'Hatchback', 'Manual', 'Petrol', 5, 4),
+    ('Ford', 'Focus', 2017, 'Gray,Silver', 'Hatchback', 'Automatic', 'Petrol', 5, 4),
+    ('Ford', 'Focus', 2022, 'Blue,White', 'Hatchback', 'Automatic', 'Diesel', 5, 4),
+    
+    -- Chevrolet Malibu Variations
+    ('Chevrolet', 'Malibu', 2003, 'Gray,Black', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Chevrolet', 'Malibu', 2008, 'Red,Silver', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    ('Chevrolet', 'Malibu', 2014, 'White,Gray', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('Chevrolet', 'Malibu', 2019, 'Blue,Black', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Chevrolet', 'Malibu', 2023, 'Silver,Red', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    
+    -- BMW 3 Series Variations
+    ('BMW', '3 Series', 2004, 'Black,Silver', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    ('BMW', '3 Series', 2009, 'White,Gray', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('BMW', '3 Series', 2016, 'Red,Blue', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('BMW', '3 Series', 2020, 'Silver,Black', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    ('BMW', '3 Series', 2023, 'White,Silver', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    
+    -- Audi A4 Variations
+    ('Audi', 'A4', 2004, 'Silver,Blue', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Audi', 'A4', 2009, 'Black,White', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    ('Audi', 'A4', 2013, 'Red,Gray', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('Audi', 'A4', 2017, 'Blue,Silver', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Audi', 'A4', 2022, 'Gray,Black', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    
+    -- Mercedes-Benz C-Class Variations
+    ('Mercedes-Benz', 'C-Class', 2006, 'Black,Red', 'Sedan', 'Automatic', 'Diesel', 5, 4),
+    ('Mercedes-Benz', 'C-Class', 2010, 'Silver,Gray', 'Sedan', 'Manual', 'Petrol', 5, 4),
+    ('Mercedes-Benz', 'C-Class', 2015, 'White,Blue', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('Mercedes-Benz', 'C-Class', 2019, 'Gray,Silver', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Mercedes-Benz', 'C-Class', 2023, 'Black,White', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    
+    -- Nissan Altima Variations
+    ('Nissan', 'Altima', 2007, 'Silver,Gray', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Nissan', 'Altima', 2010, 'Blue,White', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    ('Nissan', 'Altima', 2014, 'Black,Red', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('Nissan', 'Altima', 2018, 'Gray,Silver', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Nissan', 'Altima', 2022, 'Blue,Black', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    
+    -- Hyundai Sonata Variations
+    ('Hyundai', 'Sonata', 2008, 'Black,Silver', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Hyundai', 'Sonata', 2012, 'Red,White', 'Sedan', 'Manual', 'Hybrid', 5, 4),
+    ('Hyundai', 'Sonata', 2016, 'Blue,Gray', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Hyundai', 'Sonata', 2020, 'White,Silver', 'Sedan', 'Automatic', 'Diesel', 5, 4),
+    ('Hyundai', 'Sonata', 2024, 'Black,Gray', 'Sedan', 'Manual', 'Hybrid', 5, 4),
+    
+    -- Subaru Impreza Variations
+    ('Subaru', 'Impreza', 2005, 'Silver,Blue', 'Sedan', 'Manual', 'Petrol', 5, 4),
+    ('Subaru', 'Impreza', 2010, 'Black,Gray', 'Sedan', 'Automatic', 'Diesel', 5, 4),
+    ('Subaru', 'Impreza', 2014, 'Red,White', 'Sedan', 'Manual', 'Hybrid', 5, 4),
+    ('Subaru', 'Impreza', 2018, 'Gray,Silver', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Subaru', 'Impreza', 2022, 'Blue,Black', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    
+    -- Mazda 6 Variations
+    ('Mazda', '6', 2007, 'Red,Silver', 'Sedan', 'Automatic', 'Diesel', 5, 4),
+    ('Mazda', '6', 2011, 'Blue,Black', 'Sedan', 'Manual', 'Petrol', 5, 4),
+    ('Mazda', '6', 2015, 'Gray,White', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('Mazda', '6', 2019, 'Black,Red', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Mazda', '6', 2023, 'Silver,Blue', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    
+    -- Kia Optima Variations
+    ('Kia', 'Optima', 2006, 'Black,Gray', 'Sedan', 'Manual', 'Petrol', 5, 4),
+    ('Kia', 'Optima', 2010, 'Red,White', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('Kia', 'Optima', 2014, 'Blue,Silver', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    ('Kia', 'Optima', 2018, 'Gray,Black', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Kia', 'Optima', 2022, 'Silver,Red', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    
+    -- Lexus ES Variations
+    ('Lexus', 'ES', 2004, 'Black,Silver', 'Sedan', 'Manual', 'Petrol', 5, 4),
+    ('Lexus', 'ES', 2009, 'Red,Gray', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('Lexus', 'ES', 2013, 'White,Blue', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Lexus', 'ES', 2017, 'Silver,Black', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    ('Lexus', 'ES', 2021, 'Gray,Red', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    
+    -- Chrysler 300 Variations
+    ('Chrysler', '300', 2007, 'Silver,Black', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    ('Chrysler', '300', 2012, 'Gray,White', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    ('Chrysler', '300', 2016, 'Red,Blue', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('Chrysler', '300', 2020, 'Black,Silver', 'Sedan', 'Manual', 'Petrol', 5, 4),
+    ('Chrysler', '300', 2024, 'White,Gray', 'Sedan', 'Automatic', 'Diesel', 5, 4),
+    
+    -- Volkswagen Passat Variations
+    ('Volkswagen', 'Passat', 2003, 'Red,Black', 'Sedan', 'Automatic', 'Diesel', 5, 4),
+    ('Volkswagen', 'Passat', 2007, 'Blue,Silver', 'Sedan', 'Manual', 'Petrol', 5, 4),
+    ('Volkswagen', 'Passat', 2011, 'Gray,White', 'Sedan', 'Automatic', 'Hybrid', 5, 4),
+    ('Volkswagen', 'Passat', 2016, 'Silver,Black', 'Sedan', 'Manual', 'Diesel', 5, 4),
+    ('Volkswagen', 'Passat', 2020, 'Red,Blue', 'Sedan', 'Automatic', 'Petrol', 5, 4),
+    
+    -- Jeep Grand Cherokee Variations
+    ('Jeep', 'Grand Cherokee', 2005, 'Black,Gray', 'SUV', 'Automatic', 'Diesel', 5, 4),
+    ('Jeep', 'Grand Cherokee', 2010, 'Silver,White', 'SUV', 'Manual', 'Petrol', 5, 4),
+    ('Jeep', 'Grand Cherokee', 2014, 'Red,Black', 'SUV', 'Automatic', 'Hybrid', 5, 4),
+    ('Jeep', 'Grand Cherokee', 2018, 'Gray,Silver', 'SUV', 'Manual', 'Petrol', 5, 4),
+    ('Jeep', 'Grand Cherokee', 2022, 'Black,Red', 'SUV', 'Automatic', 'Diesel', 5, 4),
+    
+    -- Land Rover Range Rover Variations
+    ('Land Rover', 'Range Rover', 2005, 'Black,Gray', 'SUV', 'Automatic', 'Petrol', 5, 4),
+    ('Land Rover', 'Range Rover', 2010, 'Silver,White', 'SUV', 'Manual', 'Diesel', 5, 4),
+    ('Land Rover', 'Range Rover', 2014, 'Blue,Red', 'SUV', 'Automatic', 'Hybrid', 5, 4),
+    ('Land Rover', 'Range Rover', 2018, 'Gray,Silver', 'SUV', 'Manual', 'Diesel', 5, 4),
+    ('Land Rover', 'Range Rover', 2022, 'Black,Blue', 'SUV', 'Automatic', 'Petrol', 5, 4),
+    
+    -- Volvo XC90 Variations
+    ('Volvo', 'XC90', 2004, 'Red,Black', 'SUV', 'Manual', 'Petrol', 5, 4),
+    ('Volvo', 'XC90', 2009, 'Blue,Silver', 'SUV', 'Automatic', 'Diesel', 5, 4),
+    ('Volvo', 'XC90', 2014, 'Gray,Black', 'SUV', 'Manual', 'Hybrid', 5, 4),
+    ('Volvo', 'XC90', 2018, 'Silver,Red', 'SUV', 'Automatic', 'Petrol', 5, 4),
+    ('Volvo', 'XC90', 2022, 'White,Blue', 'SUV', 'Manual', 'Diesel', 5, 4);
--- Database/schema.sql
+++ Database/schema.sql
@@ -1,15 +1,34 @@
-CREATE TABLE Dealers (
-    DealerID INTEGER PRIMARY KEY,
-    DealerName TEXT NOT NULL,
-    DealerToken TEXT NOT NULL UNIQUE
-);
+DROP TABLE IF EXISTS Stock;
+DROP TABLE IF EXISTS Cars;
+DROP TABLE IF EXISTS Dealers;
 
+- Cars Table
 CREATE TABLE Cars (
-    CarID INTEGER PRIMARY KEY,
+    CarID INTEGER PRIMARY KEY AUTOINCREMENT,
     Make TEXT NOT NULL,
     Model TEXT NOT NULL,
     Year INTEGER NOT NULL,
-    StockLevel INTEGER NOT NULL,
+    Colours TEXT NOT NULL,
+    Body TEXT NOT NULL,
+    Transmission TEXT NOT NULL,
+    FuelType TEXT NOT NULL,
+    Seats INTEGER NOT NULL,
+    Doors INTEGER NOT NULL
+);
+    
+-- Dealers Table
+CREATE TABLE Dealers (
+    DealerID INTEGER PRIMARY KEY AUTOINCREMENT,
+    DealerName TEXT NOT NULL,
+    DealerToken TEXT NOT NULL UNIQUE
+);
+    
+-- Stock Table
+CREATE TABLE Stock (
+    CarID INTEGER NOT NULL,
     DealerID INTEGER NOT NULL,
+    StockLevel INTEGER NOT NULL,
+    PRIMARY KEY (CarID, DealerID),
+    FOREIGN KEY (CarID) REFERENCES Cars (CarID),
     FOREIGN KEY (DealerID) REFERENCES Dealers (DealerID)
 );
