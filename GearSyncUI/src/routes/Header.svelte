cd40dc074881f8312be2362d9c02f94cf2daf2a4
KashyapChilivoju <kchilivoju1@gmail.com>
2024-12-12 21:39:53 +1100

    Header Done | Redirect to root

---
+++ GearSyncUI/src/routes/Header.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+
+    function navigateHome() {
+        goto("/");
+    }
+</script>
+
+<main>
+    <header class="header" on:click={navigateHome} style="cursor: pointer;">
+        <img src="/favicon.png" alt="GearSync Logo" class="logo" />
+        <h1>GearSync</h1>
+    </header>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-family: "Arial", sans-serif;
+        margin: 1rem;
+        background-color: #041322;
+    }
+
+    .header {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        width: 100%;
+        justify-content: center;
+        border-bottom: 2px solid #ddd;
+        padding: 1rem 0;
+        cursor: pointer;
+    }
+
+    .logo {
+        width: 250px;
+        height: auto;
+    }
+
+    h1 {
+        font-size: 7rem;
+        color: #D8DBDE;
+        margin: 0;
+    }
+</style>
